03f0cd8577475bd57abdabd48432ed5c09f0c010
Jeril George Kadavan <jerilgkadavan@gmail.com>
2019-03-13 13:33:05 -0400

    user

---
+++ data.js
@@ -0,0 +1,20 @@
+const data = {
+    name: "jeril"
+    role: "Designer",
+
+    user1:{
+        name: "user1",
+        role: "developer",
+        bio: "some bio goes here",
+        bioPic: ""
+    }
+
+    user2:{
+        name: "user2",
+        role: "backend",
+        bio: "some bio goes here",
+        bioPic: ""
+    }
+};
+
+module.exports = data;
